a0ee4194220fdfd7038bdc4cea4bbc0524e2116b
Pedro Simoes <pedro.simoes@gmail.com>
2019-12-20 21:35:28 -0300

    Imported Alamofire Minimum macOS 10.12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "tolarianbot",
-    
+    platforms: [
+       .macOS(.v10_12),
+       ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
@@ -16,12 +18,13 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/zmeyc/telegram-bot-swift.git", from: "1.1.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "tolarianbot",
-            dependencies: ["TelegramBotSDK"]),
+            dependencies: ["TelegramBotSDK", "Alamofire"]),
     ]
 )
--- Sources/tolarianbot/main.swift
+++ Sources/tolarianbot/main.swift
@@ -1,14 +1,28 @@
 import Foundation
 import TelegramBotSDK
+import Alamofire
 
 let token = readToken(from: "tolarianbotToken")
 let bot = TelegramBot(token: token)
+var query = String()
 
+// Bot main loop. Gets updates and sends replies
 while let update = bot.nextUpdateSync() {
-    if let message = update.message, let from = message.from, let text = message.text {
+//    This part deals with messages sent directly to a chat window with the bot
+    if let message = update.message,
+        let from = message.from,
+        let text = message.text {
+//      TODO: Change the message the user gets from interacting with the bot to something more useful
         bot.sendMessageAsync(chatId: from.id,
                              text: "Hi \(from.firstName)! You said: \(text).\n")
     }
+//    This deals with queries sent to the bot via its inline mode (e.g.: "@tolarianbot Garruk")
+    if let query = update.inlineQuery?.query.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) {
+//        func getSmallImages (url: String, parameters: [String: String]) {
+//            Alamofire.request(url, method: .get, paramenters: parameters).responseJSON {
+//
+//            }
+        }
 }
 
 fatalError("Server stopped due to error: \(String(describing: bot.lastError))")
