2bbc439b8d6be7b149c7d1109f20299acf2983bf
Pedro Simoes <pedro.simoes@gmail.com>
2019-12-22 14:01:24 -0300

    Deleted code for chat window bot usage (not inline) Scryfall search working and returning correct results

--- Sources/tolarianbot/main.swift
+++ Sources/tolarianbot/main.swift
@@ -4,30 +4,18 @@ import Alamofire
 
 let token = readToken(from: "tolarianbotToken")
 let bot = TelegramBot(token: token)
-var query = String()
 
 // Bot main loop. Gets updates and sends replies
 while let update = bot.nextUpdateSync() {
-//    This part deals with messages sent directly to a chat window with the bot
-    if let message = update.message,
-        let from = message.from,
-        let text = message.text {
-//      TODO: Change the message the user gets from interacting with the bot to something more useful
-        bot.sendMessageAsync(chatId: from.id,
-                             text: "Hi \(from.firstName)! You said: \(text).\n")
-    }
-//    This deals with queries sent to the bot via its inline mode (e.g.: "@tolarianbot Garruk")
-    if let query = update.inlineQuery?.query {
-        print("Inline query received")
-            }
-}
 
-func searchScryfall (parameters: Parameters) {
-    let parameters = ["foo", "bar"]
-    AF.request("https://api.scryfall.com").responseJSON(completionHandler: { response in
-        debugPrint(response)
-        let searchResult = response.data
-    })
+//    This deals with queries sent to the bot via its inline mode (e.g.: "@tolarianbot Garruk")
+    if let query = update.inlineQuery?.query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+        print("Inline query received: \(query)")
+//        Sends the search terms to the Scryfall API
+        AF.request("https://api.scryfall.com/cards/search?q=\(query)").responseJSON(completionHandler: { response in
+            debugPrint(response)
+        })
+            }
 }
 
 fatalError("Server stopped due to error: \(String(describing: bot.lastError))")
