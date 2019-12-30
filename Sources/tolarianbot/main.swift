798fcc07191002e0e641946aaccbfafc6db17e49
Pedro Simoes <pedro.simoes@gmail.com>
2019-12-30 01:36:13 -0300

    Switched to async responses

--- Sources/tolarianbot/main.swift
+++ Sources/tolarianbot/main.swift
@@ -10,26 +10,26 @@ let telegramMaxResponseSize = 50
 // Bot main loop. Gets updates and sends replies
 while let update = bot.nextUpdateSync() {
 
-//    This deals with queries sent to the bot via its inline mode (e.g.: "@tolarianbot Garruk")
+//  This deals with queries sent to the bot via its inline mode (e.g.: "@tolarianbot Garruk")
     let queryID = update.inlineQuery?.id
     if let query = update.inlineQuery?.query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
         print("Inline query received: ID \(queryID!) - \(query)")
         
-//        Sends the search terms to the Scryfall API
+//      Sends the search terms to the Scryfall API
         AF.request("https://api.scryfall.com/cards/search?q=\(query)").responseJSON(completionHandler: { response in
             
-//            Constructs a json SwiftyJSON object from Scryfall's response
+//          Constructs a json SwiftyJSON object from Scryfall's response
             switch response.result {
             case .failure(let error):
                 print(error)
             case .success(_):
                 let json = try? JSON(data: response.data!)
                 
-//                Variables that will comprise the bot's response
+//              Variables that will comprise the bot's response
                 var cardResult:InlineQueryResultPhoto = InlineQueryResultPhoto.init()
                 var inlineBotResponse:[InlineQueryResultPhoto] = []
                 
-//                Extracts relevant data from the constructed json, up to Telegram's max number of items in a response
+//              Extracts relevant data from the constructed json, up to Telegram's max number of items in a response
                 for card in json!["data"].arrayValue.prefix(telegramMaxResponseSize) {
                     guard let cardID = card["id"].string else {return}
                     guard let bigImage = card["image_uris"]["large"].string else {return}
@@ -45,7 +45,7 @@ while let update = bot.nextUpdateSync() {
                         
                 }
 //              Sends the bot's response
-                bot.answerInlineQuerySync(inlineQueryId: queryID!, results:inlineBotResponse)
+                bot.answerInlineQueryAsync(inlineQueryId: queryID!, results:inlineBotResponse)
             }
         })
     }
